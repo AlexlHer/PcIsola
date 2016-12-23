870745518d3ba1e3939b6ca3bc19a36e2f04d436
Alexandre <alexandre.alex10@hotmail.fr>
2016-12-23 14:43:09 +0100

    Fonctions secondaires (version 5.0 et supérieur)

--- fonctions_isola.h
+++ fonctions_isola.h
@@ -22,7 +22,9 @@ Tab2Dint deplacement(Tab2Dint t, int numnom, string ou);
 Tab2Dint casse_bloc(Tab2Dint t);
 bool possibl_deplac(Tab2Dint t, int numnom);
 bool In(Tab1Dstring t, string cherche);
-void affichage_deplacement_possible(Tab1Dstring t);
+void affichage_deplacement_possible(Tab1Dstring t, int numnom);
 void sauvegarde(string type_de_save, string type_de_jeu, Tab2Dint tab_de_jeu, int nb_tour, string nom1, string nom2, string gagnant);
+Tab1Dint position_pion(Tab2Dint t, int numnom);
+Tab1Dstring deplacement_possible(Tab2Dint t, int x, int y);
 
-#endif //FONCTIONS_ISOLA_H_INCLUDED 
+#endif //FONCTIONS_ISOLA_H_INCLUDED
