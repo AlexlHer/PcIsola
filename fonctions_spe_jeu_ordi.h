7ddc7df49e735ea33d173823e1b6f2f2cb9d2c58
Alexandre <alexandre.alex10@hotmail.fr>
2016-12-23 14:46:03 +0100

    Fonctions secondaires (version 3.1 et supérieur)

--- fonctions_spe_jeu_ordi.h
+++ fonctions_spe_jeu_ordi.h
@@ -11,5 +11,10 @@ typedef vector<vector<int>> Tab2Dint;
 
 string demande_deplace_ordi(Tab2Dint t, int numnom);
 Tab2Dint casse_bloc_ordi(Tab2Dint t);
+int superieur(Tab1Dint t);
+int position_tableau(Tab2Dint tab, int numnom);
+Tab1Dint comptage_dans_tableau(Tab1Dint tab, int x);
+int alea_dans_tab(Tab1Dint tab);
 
-#endif //FONCTIONS_SPE_JEU_ORDI_H_INCLUDED 
+
+#endif //FONCTIONS_SPE_JEU_ORDI_H_INCLUDED
